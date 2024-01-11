8111553eddee7c81478904404c032ee3bb50a352
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2024-01-11 15:54:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+(function () {
+  // .............SWITCHER and FORM TRANSLATE X................
+
+  const switcher = document.getElementById('switch');
+  const switchMark = document.querySelector('.acces-section__switcher label');
+  const regInForm = document.querySelector('.action-forms__reg-in');
+  const logInForm = document.querySelector('.action-forms__log-in');
+  const regForms = document.querySelector('.action-forms');
+
+  switcher.addEventListener('click', ({ target: { checked } }) => {
+    if (checked) {
+      switchMark.classList.add('checked');
+      regInForm.reset();
+      regForms.classList.add('move');
+    } else {
+      switchMark.classList.remove('checked');
+      logInForm.reset();
+      regForms.classList.remove('move');
+    }
+  });
+
+  // PASSWORD VISABILITY
+
+  const passwordSections = document.querySelectorAll(
+    '.action-forms__password-box'
+  );
+
+  passwordSections.forEach((passwordSection) => {
+    const passwordInput = passwordSection.querySelector('input');
+    const passwordInputSvg = passwordSection.querySelector('svg');
+    let isPasswordVisible = false;
+
+    function showPassword() {
+      isPasswordVisible = true;
+      passwordInput.type = 'text';
+      passwordInput.style.color = '#fff';
+      passwordInputSvg.classList.add('visible');
+      // passwordInput.focus();
+    }
+
+    function hidePassword() {
+      isPasswordVisible = false;
+      passwordInput.type = 'password';
+      passwordInput.style.color = '#153416';
+      passwordInputSvg.classList.remove('visible');
+      // passwordInput.focus();
+    }
+
+    passwordSection.addEventListener('click', ({ target }) => {
+      if (target === passwordInputSvg) {
+        isPasswordVisible ? hidePassword() : showPassword();
+      }
+    });
+    // debugger
+    document.addEventListener('click', ({ target }) => {
+      if (!passwordInputSvg.contains(target)) {
+        hidePassword();
+      }
+    });
+  });
+
+  //.................Placeholder...............
+
+  const previousPlaceholders = new Map();
+
+  document.addEventListener(
+    'focus',
+    (event) => {
+      const { target } = event;
+      if (target.tagName === 'INPUT' && target.placeholder) {
+        previousPlaceholders.set(target, target.placeholder);
+        target.placeholder = '';
+      }
+    },
+    true
+  );
+
+  document.addEventListener(
+    'blur',
+    (event) => {
+      const { target } = event;
+      if (target.tagName === 'INPUT' && previousPlaceholders.has(target)) {
+        target.placeholder = previousPlaceholders.get(target);
+        previousPlaceholders.delete(target);
+      }
+    },
+    true
+  );
+})();
---
+++ modal_alert.js
@@ -0,0 +1,156 @@
+// ............MODAL ALERT ...............
+
+const storedUsers = localStorage.getItem('usersSet');
+const usersSet = new Set(storedUsers ? JSON.parse(storedUsers) : []);
+
+function isUserExist(email) {
+  for (const user of usersSet) {
+    if (user.email === email) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getCurrentUser(email) {
+  for (const user of usersSet) {
+    return user.email === email ? user : null;
+  }
+}
+
+function isPasswordMatch(logInEmail, logInPassword) {
+  const user = getCurrentUser(logInEmail);
+  return user !== null && user.password === logInPassword;
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function showAlert(title, text) {
+  modalAlert.hidden = false;
+  modalAlert.innerHTML = '';
+  modalAlert.innerHTML = `<div class="modal-alert__alert">
+                <div class="modal-alert__header">
+                    <p>${title}</p>
+                </div>
+                <div class="modal-alert__body">
+                    <p>${text}</p>
+                </div>
+                <span class="modal-alert__btn">Ok</span>
+            </div>`;
+
+  const modalAlertBtn = document.querySelector('.modal-alert__btn');
+  modalAlertBtn.addEventListener('click', () => {
+    modalAlert.hidden = true;
+  });
+}
+
+// Registration
+
+const registrationForm = document.querySelector('.action-forms__reg-in');
+const modalAlert = document.querySelector('.modal-alert');
+const regFormButton = registrationForm.querySelector('input[type="submit"]');
+
+modalAlert.hidden = true;
+
+regFormButton.addEventListener('click', (event) => {
+  debugger;
+  event.preventDefault();
+  let title = '';
+  let text = '';
+  const email = registrationForm
+    .querySelector('input[type="email"]')
+    .value.trim();
+  const firstName = registrationForm.querySelector('#firstName').value.trim();
+  const secondName = registrationForm.querySelector('#secondName').value.trim();
+  const nickname = registrationForm.querySelector('#nickname').value.trim();
+  const password = registrationForm.querySelector('#password').value.trim();
+  const passwordConfirm = registrationForm
+    .querySelector('#passwordConfirm')
+    .value.trim();
+
+  let isRegistrationSucceed = false;
+
+  if (
+    isValidEmail(email) &&
+    !isUserExist(email) &&
+    firstName &&
+    secondName &&
+    nickname &&
+    password &&
+    passwordConfirm
+  ) {
+    if (password === passwordConfirm) {
+      const newUser = {
+        email: email,
+        name: firstName,
+        secondName: secondName,
+        nickname: nickname,
+        password: password,
+      };
+      usersSet.add(newUser);
+      localStorage.setItem('usersSet', JSON.stringify(Array.from(usersSet)));
+
+      title = `Registration of <span class="bold">${nickname}</span> is successfull.`;
+      text = `Welcome ${firstName} ${secondName}`;
+
+      isRegistrationSucceed = true;
+      showAlert(title, text);
+      if (isRegistrationSucceed) {
+        registrationForm.reset();
+      }
+    } else {
+      showAlert('Error', "Passwords don't match");
+    }
+  } else {
+    if (
+      !email &&
+      !firstName &&
+      !secondName &&
+      !nickname &&
+      !password &&
+      !passwordConfirm
+    ) {
+      showAlert('Error', 'All fields required for entry');
+    } else if (!isValidEmail(email)) {
+      title = `Invalid email`;
+      text = `Enter valid email`;
+      showAlert(title, text);
+    } else if (isUserExist(email)) {
+      showAlert('Error', `This email: ${email} is alreday exist`);
+    }
+  }
+});
+
+// Login
+
+const logInForm = document.querySelector('.action-forms__log-in');
+const logInButton = logInForm.querySelector('input[type="submit"]');
+
+logInButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const logInEmail = logInForm
+    .querySelector('input[type="email"]')
+    .value.trim();
+  const logInPassword = logInForm
+    .querySelector('input[type="password"]')
+    .value.trim();
+  let title = '';
+  let text = '';
+
+  if (logInEmail && logInPassword) {
+    if (isUserExist(logInEmail) && isPasswordMatch(logInEmail, logInPassword)) {
+      const currentUser = getCurrentUser(logInEmail);
+      title = `Log-in sucseed`;
+      text = `Welcomt back, ${currentUser.nickname}`;
+      showAlert(title, text);
+    } else {
+      showAlert(`Log-in failed`, `Wrong email or password`);
+    }
+  } else {
+    showAlert(`Log-in failed`, `All fields required for entry`);
+  }
+});
